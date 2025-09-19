ef68ed4969924b4f79a015871177ef2bbc26ee8e
RashmikaD2001 <rashmika.d2167@gmail.com>
2025-09-19 11:53:58 +0530

    first commit car rental

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        {/* <a href='/login'>Login</a>
+        <a href='/register'>Register</a> */}
+        <>App</>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+        <main>
+          <Outlet />
+        </main>
+    </>
+  )
+}
+
+export default Layout
+
+/*
+
+This layout would always be available across all
+
+*/
