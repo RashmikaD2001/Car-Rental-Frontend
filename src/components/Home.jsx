6d5e3621045cfe09757f412a992eba3004eeb2c7
RashmikaD2001 <rashmika.d2167@gmail.com>
2025-10-05 23:48:25 +0530

    video section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import CarSearch from './CarSearch'
 import FeaturedCars from './FeaturedCars'
+import VideoSection from './VideoSection'
 
 const Home = () => {
   return (
     <>
       <CarSearch />
       <FeaturedCars />
+      <VideoSection />
     </>
   )
 }
